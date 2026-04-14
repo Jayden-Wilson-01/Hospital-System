953a5a5300b4be56146f9570b68ac6a01204f94c
Jayden-Wilson-2007 <Jayden@qyrotech.co.uk>
2026-04-14 16:13:22 +0100

    Other changes

---
+++ Hospital System/src/Main.java
@@ -0,0 +1,15 @@
+import BusinessLogic.*;
+import Models.Prescription;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //PatientBusinessLogic.LoadAllPatients();
+        //DoctorBusinessLogic.LoadAllDoctors();
+        //InsuranceBusinessLogic.LoadAllInsurance();
+        //DrugBusinessLogic.LoadAllDrugs();
+        //VisitBusinessLogic.LoadAllVisits();
+        PrescriptionBusinessLogic.LoadAllPrescriptions();
+    }
+}
